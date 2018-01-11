90d8ec2dcde22af71b5809545a90dfd7c22c1e3a
Manoj <manoj.mk27@gmail.com>
2018-01-11 01:06:35 +0100

    Advent of code day 1 part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -6,10 +6,11 @@ fn main() {
     let mut f = File::open("input.txt").expect("Unable to open");
 
     f.read_to_string(&mut input).expect("Something went wrong");
-    println!("{}", sum(&input));
+    println!("{}", sum1(&input));
+    println!("{}", sum2(&input));
 }
 
-fn sum(input: &str) -> u32 {
+fn sum1(input: &str) -> u32 {
     let mut total = 0;
 
     for pair in input.chars().zip(input.chars().cycle().skip(1)) {
@@ -22,23 +23,62 @@ fn sum(input: &str) -> u32 {
     total
 }
 
+
+fn sum2(input: &str) -> u32 {
+    let mut total = 0;
+
+    for pair in input.chars().zip(input.chars().cycle().skip(input.len()/2)) {
+        let (x, y) = pair;
+        if x == y {
+            total += x.to_digit(10).unwrap();
+        }
+    }
+
+    total
+}
+
+
+
+// Part 1
+
 #[test]
 fn example_one() {
-    // 1122 => 3
-    assert_eq!(sum("1122"), 3);
+    assert_eq!(sum1("1122"), 3);
 }
 
 #[test]
 fn example_two() {
-    assert_eq!(sum("1111"), 4);
+    assert_eq!(sum1("1111"), 4);
 }
 
 #[test]
 fn example_three() {
-    assert_eq!(sum("1234"), 0);
+    assert_eq!(sum1("1234"), 0);
 }
 
 #[test]
 fn example_four() {
-    assert_eq!(sum("91212129"), 9);
+    assert_eq!(sum1("91212129"), 9);
+}
+
+// Part 2
+
+#[test]
+fn example1() {
+    assert_eq!(sum2("1212"), 6);
+}
+
+#[test]
+fn example2() {
+    assert_eq!(sum2("1221"), 0);
+}
+
+#[test]
+fn example3() {
+    assert_eq!(sum2("123425"), 4);
+}
+
+#[test]
+fn example4() {
+    assert_eq!(sum2("123123"), 12);
 }
