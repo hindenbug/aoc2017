22365c29593f2509476e777fb34be3cb997393ea
Manoj <manoj.mk27@gmail.com>
2018-01-15 00:37:35 +0100

    Advent of code day 2 part 2 [looks ugly]

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -2,7 +2,8 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    println!("{}", checksum1("input.txt"));
+    println!("{}", checksum1("input1.txt"));
+    println!("{}", checksum2("input2.txt"));
 }
 
 fn checksum1(filename: &str) -> u32 {
@@ -20,8 +21,42 @@ fn checksum1(filename: &str) -> u32 {
     checksums.iter().fold(0, |acc, &x| acc + x)
 }
 
-#[test]
+fn checksum2(filename: &str) -> u32 {
+    let f = BufReader::new(File::open(filename).expect("Cannot open file"));
+    let mut checksums: Vec<u32> = Vec::new();
 
+    for line in f.lines() {
+        let mut v: Vec<u32> = line.unwrap()
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+
+        'outer: loop {
+            let x = v.pop().expect("Some Error");
+
+            for i in 0..(v.len()) {
+                let y = v[i];
+
+                if x > y && x % y == 0 {
+                    checksums.push(x / y);
+                    break 'outer;
+                } else if y > x && y % x == 0 {
+                    checksums.push(y / x);
+                    break 'outer;
+                }
+            }
+        }
+    }
+
+    checksums.iter().fold(0, |acc, &x| acc + x)
+}
+
+#[test]
 fn example1() {
     assert_eq!(checksum1("example1.txt"), 18)
+}
+
+#[test]
+fn example2() {
+    assert_eq!(checksum2("example2.txt"), 9)
 }
