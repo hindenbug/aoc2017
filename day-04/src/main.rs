37a5858109a433cdc1cce35e4285965bc96c3ec2
Manoj <manoj.mk27@gmail.com>
2018-01-21 13:38:03 +0100

    Advent of code day 4 part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -8,15 +8,16 @@ fn main() {
 
     f.read_to_string(&mut input).expect("Something went wrong");
 
-    println!("{}", input.lines().filter(|line| is_valid(line)).count())
+    println!("{}", input.lines().filter(|line| is_valid_1(line)).count());
+    println!("{}", input.lines().filter(|line| is_valid_2(line)).count())
 }
 
-fn is_valid(passphrase: &str) -> bool {
+fn is_valid_1(passphrase: &str) -> bool {
 
     let mut freq = HashMap::new();
-    let phrase = passphrase.split_whitespace();
+    let words = passphrase.split_whitespace();
 
-    for word in phrase {
+    for word in words {
         let counter = freq.entry(word).or_insert(0);
         *counter += 1;
     }
@@ -25,18 +26,88 @@ fn is_valid(passphrase: &str) -> bool {
 
 }
 
+fn is_valid_2(passphrase: &str) -> bool {
+
+    let words = passphrase.split_whitespace();
+
+    words.clone().enumerate().all(|(i1, word)| {
+        for (i2, w) in words.clone().enumerate() {
+            if i1 == i2 { continue; }
+            if is_anagram(word, w) { return false }
+        }
+        true
+    })
+
+}
+
+
+fn is_anagram(word1: &str, word2: &str) -> bool {
+    if word1.len() != word2.len() {
+        return false
+    }
+
+    let mut freq = HashMap::new();
+
+    // could have used `sort` to make it easier, wanted to try another approach.
+
+    for word in word1.chars() {
+        let counter = freq.entry(word).or_insert(0);
+        *counter +=1;
+    }
+
+    for word in word2.chars() {
+        let counter = freq.entry(word).or_insert(0);
+        *counter -= 1;
+    }
+
+    freq.values().all(|&x| x == 0 )
+}
+
 
 #[test]
+fn test_is_anagram1() {
+    assert_eq!(is_anagram("abcde", "ecdab"), true)
+}
+
+#[test]
+fn test_is_anagram2() {
+    assert_eq!(is_anagram("abcde", "fghij"), false)
+}
+
+// Tests for Part 1
+#[test]
 fn example_1() {
-    assert_eq!(is_valid("aa bb cc dd ee"), true)
+    assert_eq!(is_valid_1("aa bb cc dd ee"), true)
 }
 
 #[test]
 fn example_2() {
-    assert_eq!(is_valid("aa bb cc dd aa"), false)
+    assert_eq!(is_valid_1("aa bb cc dd aa"), false)
 }
 
 #[test]
 fn example_3() {
-    assert_eq!(is_valid("aa bb cc dd aaa"), true)
+    assert_eq!(is_valid_1("aa bb cc dd aaa"), true)
+}
+
+// Tests for Part 2
+
+#[test]
+fn example_4() {
+    assert_eq!(is_valid_2("abcde fghij"), true)
+}
+
+#[test]
+fn example_5() {
+    assert_eq!(is_valid_2("abcde xyz ecdab"), false)
+}
+
+#[test]
+fn example_6() {
+    assert_eq!(is_valid_2("a ab abc abd abf abj"), true)
+}
+
+#[test]
+fn example_7() {
+    assert_eq!(is_valid_2("iiii oiii ooii oooi oooo"), true)
 }
