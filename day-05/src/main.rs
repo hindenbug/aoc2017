785d7c4e18ba410a8b99e9ee9ee5eb3302cf349f
Manoj <manoj.mk27@gmail.com>
2018-01-22 21:24:05 +0100

    Advent of code day 5 part 2

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,16 +1,24 @@
 fn main() {
     let input = include_str!("../input.txt");
-    println!("{:?}", step_count_for(input));
+    // increase offset by 1 for part 1
+    println!("{:?}", step_count_for(input, false));
+
+    //decrease offset by 1 part 2
+    println!("{:?}", step_count_for(input, true));
 }
 
-fn step_count_for(input: &str) -> usize {
+fn step_count_for(input: &str, decrease: bool) -> usize {
     let mut steps = 0;
     let mut instructions: Vec<i32> = input.lines().map(|x| x.parse::<i32>().unwrap()).collect();
     let mut current_offset: i32 = 0;
 
     while let Some(offset) = instructions.get_mut(current_offset as usize) {
         current_offset += *offset;
-        *offset += 1;
+        if decrease && *offset >= 3 {
+            *offset -= 1
+        } else {
+            *offset += 1
+        }
         steps += 1;
     }
 
@@ -20,5 +28,11 @@ fn step_count_for(input: &str) -> usize {
 #[test]
 fn example_1() {
     let input = include_str!("../example.txt");
-    assert_eq!(step_count_for(input), 5)
+    assert_eq!(step_count_for(input, false), 5)
+}
+
+#[test]
+fn example_2() {
+    let input = include_str!("../example.txt");
+    assert_eq!(step_count_for(input, true), 10)
 }
