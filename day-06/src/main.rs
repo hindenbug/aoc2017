038183fb176455e8d57e36570fffe62c16a5134f
hindenbug <manoj.mk27@gmail.com>
2018-01-27 19:56:04 +0100

    Advent of code day 6 part 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,13 +1,14 @@
 use std::collections::HashSet;
-
+use std::collections::HashMap;
 
 fn main() {
     let input = include_str!("../input.txt");
-    println!("{:?}", count_cycles_for(input));
+    println!("{:?}", redistribute_1(input));
+    println!("{:?}", redistribute_2(input));
 
 }
 
-fn count_cycles_for(input: &str) -> usize {
+fn redistribute_1(input: &str) -> usize {
     let mut bank = input
         .split_whitespace()
         .map(|x| x.parse::<usize>().unwrap())
@@ -20,7 +21,6 @@ fn count_cycles_for(input: &str) -> usize {
 
     loop {
         let (mut index, max) = find_max_with_index(bank.clone());
-        println!("{:?}", bank.clone());
 
         let mut memory = max;
         bank[index] -= max;
@@ -39,6 +39,41 @@ fn count_cycles_for(input: &str) -> usize {
     cycles
 }
 
+fn redistribute_2(input: &str) -> usize {
+    let mut bank = input
+        .split_whitespace()
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut previous: HashMap<Vec<usize>, usize> = HashMap::new();
+    let mut cycles = 0;
+
+    previous.insert(bank.clone(), cycles);
+
+    loop {
+        let (mut index, max) = find_max_with_index(bank.clone());
+
+        let mut memory = max;
+        bank[index] -= max;
+
+        for idx in (0..bank.len()).cycle().skip(index+1) {
+            bank[idx] += 1;
+            memory -= 1;
+            if memory == 0 { break;}
+        }
+
+        cycles += 1;
+
+        if previous.contains_key(&bank.clone()) {
+            break;
+        }
+
+        previous.insert(bank.clone(), cycles);
+    }
+    cycles - previous.get(&bank).unwrap()
+}
+
+
 fn find_max_with_index(bank: Vec<usize>) -> (usize, usize) {
     let (i, max) = bank.iter().enumerate().max_by(|x, y| x.1.cmp(y.1).then_with(|| y.0.cmp(&x.0))).unwrap();
     (i, *max)
@@ -46,9 +81,15 @@ fn find_max_with_index(bank: Vec<usize>) -> (usize, usize) {
 }
 
 #[test]
-fn example() {
-    let input = include_str!("../input.txt");
-    assert_eq!(count_cycles_for(input), 5);
+fn example_1() {
+    let input = include_str!("../example.txt");
+    assert_eq!(redistribute_1(input), 5);
+}
+
+#[test]
+fn example_2() {
+    let input = include_str!("../example.txt");
+    assert_eq!(redistribute_2(input), 4);
 }
 
 #[test]
