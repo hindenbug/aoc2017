9aa065f6512588fd558414faa6e7dce86212cff7
hindenbug <manoj.mk27@gmail.com>
2018-02-04 23:36:12 +0100

    [WIP] Advent of code day 7 part 1

---
+++ day-07/src/main.rs
@@ -0,0 +1,42 @@
+extern crate regex;
+
+use regex::Regex;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Program {
+    name: String,
+    weight: i32,
+    children: Vec<String>,
+    parent: Option<String>
+}
+
+fn main() {
+    let input = include_str!("../example.txt");
+    root(input);
+}
+
+fn root(input: &str) -> &str {
+    let re = Regex::new(r"(\w*) \((\d+)\)( -> ([\w, ]*)+)").unwrap();
+    let mut programs = HashMap::new();
+
+    for line in input.lines() {
+        for capture in re.captures_iter(line) {
+            let mut program = Program {
+                name: capture[1].to_string(),
+                weight: capture[2].parse().unwrap(),
+                children: capture[4].split(", ").map(|s| s.to_string()).collect::<Vec<_>>(),
+                parent: None,
+            };
+            programs.insert(program.name.clone(), program);
+        }
+    }
+
+    "tknk"
+}
+
+#[test]
+fn example() {
+    let input = include_str!("../example.txt");
+    assert_eq!(root(input), "tknk")
+}
