568d248454e6c0dc5378eba865178f0219b4b702
hindenbug <manoj.mk27@gmail.com>
2018-02-10 23:08:20 +0100

    Advent of code day 7 part 1

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -12,30 +12,50 @@ struct Program {
 }
 
 fn main() {
-    let input = include_str!("../example.txt");
-    root(input);
+    let input = include_str!("../input.txt");
+    println!("{:#?}", root(input));
 }
 
-fn root(input: &str) -> &str {
+fn root(input: &str) -> String {
     let re = Regex::new(r"(\w*) \((\d+)\)( -> ([\w, ]*)+)").unwrap();
     let mut programs = HashMap::new();
+    let mut nodes = Vec::new();
+    let mut root = String::new();
 
     for line in input.lines() {
         for capture in re.captures_iter(line) {
+            let mut children = capture[4]
+                .split(", ")
+                .map(|s| s.to_string())
+                .collect::<Vec<_>>();
+
             let mut program = Program {
                 name: capture[1].to_string(),
                 weight: capture[2].parse().unwrap(),
-                children: capture[4]
-                    .split(", ")
-                    .map(|s| s.to_string())
-                    .collect::<Vec<_>>(),
+                children: children,
                 parent: None,
             };
-            programs.insert(program.name.clone(), program);
+
+            programs.insert(program.name.to_owned(), program.children);
         }
     }
 
-    "tknk"
+
+    // This is ugly, must be a better way
+
+    for (_k, v) in programs.iter() {
+        nodes.push(v)
+    }
+
+    let nodes = nodes.iter().flat_map(|&x| x).collect::<Vec<_>>();
+
+    for (k, _v) in programs.iter() {
+        if !nodes.contains(&k) {
+            root = k.to_string();
+        }
+    }
+
+    root
 }
 
 #[test]
