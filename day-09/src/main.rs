78a960e1275451f542e9667b7e66526f2929ba9a
hindenbug <manoj.mk27@gmail.com>
2018-02-13 22:06:45 +0100

    Advent of code day 9 part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -2,12 +2,13 @@ static INPUT: &'static str = include_str!("../input.txt");
 
 fn main() {
     let input = INPUT;
-    println!("{:#?}", part1(input));
+    println!("{:#?}", score(input));
 }
 
-fn part1(input: &str) -> u32 {
+fn score(input: &str) -> (u32, u32) {
     let mut total = 0;
     let mut depth = 0;
+    let mut garbage = 0;
     let mut chrs = input.chars();
 
     while let Some(c) = chrs.next() {
@@ -26,52 +27,52 @@ fn part1(input: &str) -> u32 {
                         chrs.next();
                     }
                     '>' => break,
-                    _ => {}
+                    _ => garbage += 1,
                 }
-            },
-            _ => {}
+            }
+            _ => {},
         }
     }
 
-    total
+    (total, garbage)
 }
 
 #[test]
-fn part1_test_1() {
-    assert_eq!(part1("{}"), 1)
+fn score_test_1() {
+    assert_eq!(score("{}"), 1)
 }
 
 #[test]
-fn part1_test_2() {
-    assert_eq!(part1("{{{}}}"), 6)
+fn score_test_2() {
+    assert_eq!(score("{{{}}}"), 6)
 }
 
 #[test]
-fn part1_test_3() {
-    assert_eq!(part1("{{},{}}"), 5)
+fn score_test_3() {
+    assert_eq!(score("{{},{}}"), 5)
 }
 
 #[test]
-fn part1_test_4() {
-    assert_eq!(part1("{{{},{},{{}}}}"), 16)
+fn score_test_4() {
+    assert_eq!(score("{{{},{},{{}}}}"), 16)
 }
 
 #[test]
-fn part1_test_5() {
-    assert_eq!(part1("{<a>,<a>,<a>,<a>}"), 1)
+fn score_test_5() {
+    assert_eq!(score("{<a>,<a>,<a>,<a>}"), 1)
 }
 
 #[test]
-fn part1_test_6() {
-    assert_eq!(part1("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9)
+fn score_test_6() {
+    assert_eq!(score("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9)
 }
 
 #[test]
-fn part1_test_7() {
-    assert_eq!(part1("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9)
+fn score_test_7() {
+    assert_eq!(score("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9)
 }
 
 #[test]
-fn part1_test_8() {
-    assert_eq!(part1("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3)
+fn score_test_8() {
+    assert_eq!(score("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3)
 }
