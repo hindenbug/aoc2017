4a2a4e78f186beefcba708479d123618ce356518
hindenbug <manoj.mk27@gmail.com>
2018-02-17 23:21:39 +0100

    [WIP] Advent of code day 10 part 1

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,3 +1,29 @@
+static INPUT: &'static str = include_str!("../input.txt");
+static EXAMPLE: &'static str = include_str!("../example.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let input = INPUT;
+    println!("{:#?}", knot_hash(input));
+}
+
+fn knot_hash(input: &str) -> u32 {
+    let lengths = input.trim().split(",").map(|x| x.parse::<u8>().unwrap()).collect::<Vec<u8>>();
+    let (mut current_pos, mut skip_size) = (0, 0);
+    let mut list = (0..256).map(|x| x as u8).collect::<Vec<u8>>();
+
+    for length in lengths {
+        let mut new_list = Vec::new();
+        let (mut head, mut tail) = list.split_at_mut(current_pos);
+
+        //tail[current_pos as usize..length as usize];
+        current_pos += length as usize + skip_size;
+        skip_size += 1
+    }
+
+    12
+}
+
+#[test]
+fn knot_hash_test() {
+    assert_eq!(knot_hash(EXAMPLE), 12)
 }
