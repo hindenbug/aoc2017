0655b5e0b2d8edd0dd0cf7a417dce835b738c1d2
hindenbug <manoj.mk27@gmail.com>
2018-02-25 23:03:48 +0100

    [WIP] Advent of code day 12 part 1

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,3 +1,32 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+static INPUT: &'static str = include_str!("../input.txt");
+static EXAMPLE: &'static str = include_str!("../example.txt");
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", part1(INPUT));
+}
+
+fn part1(input: &str) -> u32 {
+    let inputs = input.trim().split(" <-> ").collect::<Vec<&str>>();
+
+    let mut m = for data in inputs.iter() {
+        let program = data[0].parse::<u32>().unwrap();
+        let connections = data[1]
+            .split(", ")
+            .map(|d| d.parse::<u32>().unwrap())
+            .collect::<Vec<_>>();
+
+        (program, connections)
+    }.collect::<HashMap<u32, Vec<u32>>>();
+
+    // WIP
+
+    6
+}
+
+#[test]
+fn test_part1() {
+    assert_eq!(part1(EXAMPLE), 6)
 }
