634a8848465c311bf49a8525e002160df8288263
hindenbug <manoj.mk27@gmail.com>
2018-03-03 22:26:48 +0100

    [WIP] Advent of code day 13 part 1

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -1,3 +1,32 @@
+use std::collections::HashMap;
+
+static INPUT: &'static str = include_str!("../input.txt");
+static EXAMPLE: &'static str = include_str!("../example.txt");
+
 fn main() {
-    println!("Hello, world!");
+    println!("#{:?}", part1(EXAMPLE));
+}
+
+fn part1(input: &str) -> usize {
+    let details = input.lines()
+        .map(|line| {
+            line.split(": ")
+                .map(|x| x.parse::<usize>().unwrap());
+
+        })
+        .collect::<Vec<_>>();
+
+    for detail in details.into_iter() {
+        detail
+    }
+
+    println!("{:?}", details);
+    24
+}
+
+
+#[test]
+
+fn test_part1() {
+    assert_eq!(part1(EXAMPLE), 24)
 }
