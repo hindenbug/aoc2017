97fdb2eac0bcd24edf3ebf22b1953f3776669a0a
hindenbug <manoj.mk27@gmail.com>
2018-03-04 22:46:35 +0100

    Advent of code day 13 part 2

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -5,6 +5,7 @@ static EXAMPLE: &'static str = include_str!("../example.txt");
 
 fn main() {
     println!("{:?}", part1(INPUT));
+    println!("{:?}", part2(INPUT));
 }
 
 fn part1(input: &str) -> u32 {
@@ -30,8 +31,42 @@ fn part1(input: &str) -> u32 {
     severity
 }
 
-#[test]
+fn part2(input: &str) -> u32 {
+    let mut delay = 0;
+    let mut caught = true;
 
+    let details: HashMap<u32, u32> = input
+        .lines()
+        .map(|line| {
+            let pts: Vec<_> = line.split(": ").collect();
+            (
+                pts[0].parse::<u32>().unwrap(),
+                pts[1].parse::<u32>().unwrap(),
+            )
+        })
+        .collect();
+
+    while caught {
+        caught = false;
+        delay += 1;
+
+        for (index, depth) in details.iter() {
+            if *depth == 1 || ((index + delay) % (depth * 2 - 2)) == 0 {
+                caught = true;
+                break;
+            }
+        }
+    }
+
+    delay
+}
+
+#[test]
 fn test_part1() {
     assert_eq!(part1(EXAMPLE), 24)
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(EXAMPLE), 10)
 }
